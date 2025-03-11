9625aab8b5abff7886be8e525f4b93cd4a58aa2c
孙亦恺 <sunyikai.s@bytedance.com>
2025-03-11 21:27:45 +0800

    fix: Modify StyledComponentsRegistry to handle client-side rendering correctly

--- src/app/registry.tsx
+++ src/app/registry.tsx
@@ -18,6 +18,8 @@ export default function StyledComponentsRegistry({
     return <>{styles}</>;
   });
 
+  if (typeof window !== 'undefined') return <>{children}</>;
+
   return (
     <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
       {children}
